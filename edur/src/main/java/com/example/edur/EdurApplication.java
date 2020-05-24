c6ff4eb29dddad9b9dc6fd7f955d6bed2964fcc7
ritabrata1995 <57553879+ritabrata1995@users.noreply.github.com>
2020-05-24 19:29:55 +0530

    Add files via upload
    First Commit

---
+++ edur/src/main/java/com/example/edur/EdurApplication.java
@@ -0,0 +1,13 @@
+package com.example.edur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdurApplication.class, args);
+	}
+
+}
---
+++ edur/src/test/java/com/example/edur/EdurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
